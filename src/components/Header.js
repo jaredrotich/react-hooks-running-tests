286cb385eb44484b9fed1fbfef94412b9bb8d3e6
jared <jaredrotich3@gmail.com>
2025-04-15 22:45:29 +0300

    updated header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Header(props) {
-      return (
+      
         
-        <h1>hello from the Header!</h1>
-      )
+      return  <h1>hello from the Header!</h1>;
+      
 }
 
 export default Header;
